2b204d0bcef8da07662e0e9ab258c03477cbca39
Stephen Gardner <stephenbgardner@gmail.com>
2019-10-25 16:08:34 -0700

    Add conversation start message to initialize thread and share its ID

--- mother.go
+++ mother.go
@@ -130,8 +130,9 @@ func (mom *Mother) createConversation(directID string, slackIDs []string, notify
 		}
 		return nil, err
 	}
+	conv.sendMessageToThread(fmt.Sprintf(mom.getMsg("sessionStartConv"), conv.ThreadID))
 	if notifyUsers {
-		conv.sendMessageToDM(mom.getMsg("sessionStart"))
+		conv.sendMessageToDM(mom.getMsg("sessionStartDirect"))
 	}
 	return conv, nil
 }
